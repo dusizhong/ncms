8d7e69889feb165b14e894922151861e60ca0e7f
stone <user.email>
2020-12-25 15:58:11 +0800

    add chinese

--- config/conf.js
+++ config/conf.js
@@ -1,5 +1,5 @@
 /*!
- * global config
+ * 全局配置
  * Copyright(c) 2020 Dusizhong.com
  * MIT Licensed
  */
@@ -9,16 +9,14 @@
 const path = require('path');
 
 
-/* custom config */
-
-const title = 'NCMS'; //your website title
-const info = 'NCMS is a clean & clear Nodejs CMS for building responsive mobile-first sites.'; //your website info
-const theme = 'default'; //default theme
-const secret = 'd4e3aafd1bfe9c0b0d3'; //your secret key
-const host = 'http://192.168.33.112'; //your host address
-const port = 3000; //default port
-
-/* custom config end */
+/* 自定义配置 */
+const title = 'NCMS'; //网站名称
+const info = 'NCMS是一款基于Nodejs开发的、用于构建响应式网站的CMS系统。它使用KOA处理异步操作，实现了更佳清晰简洁、易读的代码逻辑，具有极佳扩展性。'; //网站简介
+const theme = 'default'; //当前模板/主题
+const secret = 'd4e3aafd1bfe9c0b0d3'; //session密钥
+const host = 'http://192.168.33.112'; //域名/IP
+const port = 3000; //默认端口
+/* 自定义配置结束 */
 
 
 module.exports = {
